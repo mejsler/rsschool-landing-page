c18abd09bf882565629f6a26262844e85e9c7d11
mejsler <mejsler@pm.me>
2023-12-11 00:08:23 +0500

    feat: started working on slider

--- coffee-house/script.js
+++ coffee-house/script.js
@@ -1,23 +1,36 @@
-const menu = document.querySelector(".burger-menu");
-const burgerLink = document.querySelectorAll(".burger-link");
-const burger = document.querySelector(".burger");
-const body = document.querySelector(".body");
+const menu = document.querySelector('.burger-menu');
+const burgerLink = document.querySelectorAll('.burger-link');
+const burger = document.querySelector('.burger');
+const body = document.querySelector('.body');
+const slider = document.querySelector('.favorite-overflow');
+const width = slider.offsetWidth;
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+let step = 0;
 
+next.addEventListener('click', () => {
+  step === 2 ? step = 0 : ++step;
+  slider.style.transform = `translateX(-${width * step}px)`;
+  
+});
 
-function toggleMenu() {
-  if (menu.classList.contains("burger-show")) {
-    menu.classList.remove("burger-show");
-    body.classList.remove("overflow");
+prev.addEventListener('click', () => {
+  step === 0 ? step = 2 : --step;
+  slider.style.transform = `translateX(-${width * step}px)`;
+});
+
+const toggleMenu = () => {
+  if (menu.classList.contains('burger-show')) {
+    menu.classList.remove('burger-show');
+    body.classList.remove('overflow');
   } else {
-    menu.classList.add("burger-show");
-    body.classList.add("overflow");
+    menu.classList.add('burger-show');
+    body.classList.add('overflow');
   }
-}
+};
 
-burger.addEventListener("click", toggleMenu);
+burgerLink.forEach((burgerLink) =>
+  burgerLink.addEventListener('click', toggleMenu)
+);
 
-burgerLink.forEach( 
-    function(burgerLink) { 
-        burgerLink.addEventListener("click", toggleMenu);
-    }
-  )
+burger.addEventListener('click', toggleMenu);
