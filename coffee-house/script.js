117489b5f73d3171c7e0f19170e20ffc2a163f8c
mejsler <mejsler@pm.me>
2023-12-11 16:41:13 +0500

    feat: add touch events to slider

--- coffee-house/script.js
+++ coffee-house/script.js
@@ -2,22 +2,62 @@ const menu = document.querySelector('.burger-menu');
 const burgerLink = document.querySelectorAll('.burger-link');
 const burger = document.querySelector('.burger');
 const body = document.querySelector('.body');
+const favoriteBlock = document.querySelector('.favorite');
 const slider = document.querySelector('.favorite-overflow');
-const width = slider.offsetWidth;
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
+let width = slider.offsetWidth;
 let step = 0;
 
-next.addEventListener('click', () => {
-  step === 2 ? step = 0 : ++step;
-  slider.style.transform = `translateX(-${width * step}px)`;
-  
+// Slider
+
+window.addEventListener('resize', function () {
+  width = slider.offsetWidth;
 });
 
-prev.addEventListener('click', () => {
-  step === 0 ? step = 2 : --step;
+const nextSlide = () => {
+  step === 2 ? (step = 0) : ++step;
   slider.style.transform = `translateX(-${width * step}px)`;
-});
+};
+
+const prevSlide = () => {
+  step === 0 ? (step = 2) : --step;
+  slider.style.transform = `translateX(-${width * step}px)`;
+};
+
+next.addEventListener('click', nextSlide);
+
+prev.addEventListener('click', prevSlide);
+
+let touchstartX = 0;
+let touchendX = 0;
+
+const handleSwipe = () => {
+  if (touchendX > touchstartX) {
+    prevSlide();
+  } else if (touchendX < touchstartX) {
+    nextSlide();
+  }
+};
+
+favoriteBlock.addEventListener(
+  'touchstart',
+  (event) => {
+    touchstartX = event.changedTouches[0].screenX;
+  },
+  false
+);
+
+favoriteBlock.addEventListener(
+  'touchend',
+  (event) => {
+    touchendX = event.changedTouches[0].screenX;
+    handleSwipe();
+  },
+  false
+);
+
+// Burger
 
 const toggleMenu = () => {
   if (menu.classList.contains('burger-show')) {
